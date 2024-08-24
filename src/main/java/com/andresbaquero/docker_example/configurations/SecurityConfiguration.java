f2a01552631b19410d6dcf1d78fbdb31b536b310
AndresBaquero-00 <andres.baquero6@gmail.com>
2024-08-24 15:00:17 -0500

    Se crean archivos e imagen de Docker.

--- src/main/java/com/andresbaquero/docker_example/configurations/SecurityConfiguration.java
+++ src/main/java/com/andresbaquero/docker_example/configurations/SecurityConfiguration.java
@@ -1,7 +1,6 @@
 package com.andresbaquero.docker_example.configurations;
 
 import java.util.Arrays;
-import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
